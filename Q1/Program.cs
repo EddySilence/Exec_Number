80738e8a0806533d44897f501aee4a02823eac4a
ispan <ispan@DESKTOP-PAKSETB>
2022-10-19 16:09:29 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//題目：判斷某數值是不是質數
+			Console.WriteLine("輸入數字");
+			String input = Console.ReadLine();
+			bool Ints = int.TryParse(input, out int result);
+			double number = Math.Sqrt(result);
+			Console.WriteLine(number);
+
+			for (int i = 2; i <= number; i++)
+			{
+				if (result % i == 0)
+				{
+					Console.WriteLine("不是質數");
+					return;
+				}
+			}
+
+			//Console.WriteLine("是質數");
+		}
+	}
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//題目：找出數值區間內的所有質數
+			int numberStar = 5;
+			int numberENd = 100;
+			double number = 0;
+
+			for (int i = numberStar; i <= numberENd ; i++)
+			{
+				number = Math.Sqrt(i);
+				for (int k = 2; k <= number; k++)
+				{
+					if (i % k == 0)
+					{
+						Console.WriteLine($"{i}不是質數");
+						break;
+					}
+					else if(k == Math.Floor(number))
+					{
+						Console.WriteLine($"{i}是質數");
+					}
+				}
+			}
+
+		}
+	}
+}
