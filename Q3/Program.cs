c6b0197b54bc467af899718baacc2d3f1237ca24
eddy <eddy19991118@gmail.com>
2022-10-19 22:52:58 +0800

    finish

--- Q3/Program.cs
+++ Q3/Program.cs
@@ -16,11 +16,43 @@ namespace Q3
 				   右邊二位的數字相同
 				   這個四位數值, 剛好也是某整數的平方
 				   請問這個四位數值是什麼?*/
-			Console.WriteLine("測試");
-			Console.WriteLine("測試2");
+
+			int thousandNumber = 1;//千分位
+			int hundredNumber = 1;//百分位
+			int tenNumber = 1;//十分位
+			int oneNumber = 1;//個位
+			int result = 0;//總數
+			double sqrt = 0;//開根號
+			int integer = 0;//換成整數
+			bool isInt;//是否是整數
 
 
+            for (int i = 1; i <= 9; i++)
+			{
+                thousandNumber = thousandNumber * 1000 * i;
+                hundredNumber = hundredNumber * 100 * i;
+                for (int k = 0; k <= 9; k++)
+				{
+					tenNumber = tenNumber * 10 * k;
+					oneNumber = oneNumber * 1 * k;
 
-		}
+					result = thousandNumber + hundredNumber + tenNumber + oneNumber;
+
+					sqrt = Math.Sqrt(result);
+					isInt = int.TryParse(sqrt.ToString(), out integer);
+
+					if (isInt)
+					{
+                        Console.WriteLine($"這個數字就是 {result}");
+                    }
+
+                    tenNumber = 1;
+                    oneNumber = 1;
+                }
+                thousandNumber = 1;
+                hundredNumber = 1;
+            }
+
+        }
 	}
 }
